bb647e092e0032980161b8c10844e2b0d5d28cae
lcrazyc <144359662+lcrazyc@users.noreply.github.com>
2023-09-14 21:24:32 +0300

    Update main.py

--- Lab1/main.py
+++ Lab1/main.py
@@ -1,1 +1,26 @@
+a = int(input ("Введіть а: "))
 
+while (a < 1 or a > 100):
+
+    a = int(input ("Спройбуйте ще раз, введіть а: "))
+
+b = int(input ("Введіть b: "))
+
+while (b < 1 or b > 100):
+
+    b = int(input ("Спройбуйте ще раз, введіть b: "))
+
+
+if a > b:
+
+    r = (a*b-3)
+
+elif a == b:
+
+    r = 2
+
+else:
+
+    r = (a*a*a + 1) / b
+
+print("Результат виразу: " , r)
